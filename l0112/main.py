b22cb9ad0ac4a3bd7eba70bbc073cc71106b3ea5
demidraksin <greatraksin@icloud.com>
2020-12-01 13:25:26 +0300

    Initial commit. First lesson

--- l0112/main.py
+++ l0112/main.py
@@ -1,4 +1,6 @@
-#name = input('Введите имя: ')
+name = input('Введите имя: ')
+print(f'Hello, {name}! How are you?')
 
-#print(f'Hello, {name}! How are you?')
-
+n1 = int(input('Введи число: '))
+n2 = int(input('Введи число: '))
+print(n1 + n2)
