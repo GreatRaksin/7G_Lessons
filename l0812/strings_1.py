472d0f46dc961e33b416bba05330ffb27647aac0
demidraksin <greatraksin@icloud.com>
2020-12-08 13:24:35 +0300

    Adding strings

---
+++ l0812/strings_1.py
@@ -0,0 +1,10 @@
+a = 'ghdfkjghdkfjhgdrfg'
+b = 'Hello, I\'m a Python clan developer'
+
+print(f'Вырезал: "{b[20:24]}"')
+print(b[0:15:2])
+
+print('🗿 ' * 16, '<- Рената на матеше')
+print('2' + '2')
+
+print('mama' > 'lama')
